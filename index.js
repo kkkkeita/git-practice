303bf0978849345c24379979345f00559e3074bf
ユーザー名 <>
2023-06-23 16:11:51 +0900

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,1 @@
-<body>
-<script type="text/javascript"> //ここより下にJavaScriptコードを記述
-　let btn = document.getElementById("btn");
-　btn.addEventListener("click", function()
-{alert("Hello")}, false);
-</script> //ここより上までがJavaScriptコード
-</body>
+console.log("Hello World");
