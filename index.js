d78c35d1f4a58a69f69c820b924926e43c31b419
ユーザー名 <>
2023-06-23 15:55:38 +0900

    java

---
+++ index.js
@@ -0,0 +1,7 @@
+<body>
+<script type="text/javascript"> //ここより下にJavaScriptコードを記述
+　let btn = document.getElementById("btn");
+　btn.addEventListener("click", function()
+{alert("Hello")}, false);
+</script> //ここより上までがJavaScriptコード
+</body>
